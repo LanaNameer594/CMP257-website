598bbff1eb954e4350afe866c92a37bf60bf0815
Marim Ibrahim <6.mariam.h.6@gmail.com>
2026-04-28 22:39:41 +0400

    m

--- SQL (second version)/or-file(changed).sql
+++ SQL (second version)/or-file(changed).sql
@@ -21,13 +21,18 @@ SELECT
     CASE
         WHEN LOWER(r.recipe_name) LIKE '%chicken%' OR GROUP_CONCAT(LOWER(i.name) SEPARATOR ', ') LIKE '%chicken%' THEN 'chicken'
         WHEN LOWER(r.recipe_name) LIKE '%beef%' OR GROUP_CONCAT(LOWER(i.name) SEPARATOR ', ') LIKE '%beef%' THEN 'beef'
-        WHEN LOWER(r.recipe_name) LIKE '%fish%' OR GROUP_CONCAT(LOWER(i.name) SEPARATOR ', ') LIKE '%fish%' THEN 'fish'
+        WHEN LOWER(r.recipe_name) LIKE '%salmon%' OR GROUP_CONCAT(LOWER(i.name) SEPARATOR ', ') LIKE '%salmon%' THEN 'fish'
         WHEN LOWER(r.recipe_name) LIKE '%pasta%' OR GROUP_CONCAT(LOWER(i.name) SEPARATOR ', ') LIKE '%pasta%' THEN 'pasta'
         WHEN LOWER(r.recipe_name) LIKE '%rice%' OR GROUP_CONCAT(LOWER(i.name) SEPARATOR ', ') LIKE '%rice%' THEN 'rice'
         WHEN LOWER(r.recipe_name) LIKE '%potato%' OR GROUP_CONCAT(LOWER(i.name) SEPARATOR ', ') LIKE '%potato%' THEN 'potato'
         WHEN LOWER(r.recipe_name) LIKE '%cheese%' OR GROUP_CONCAT(LOWER(i.name) SEPARATOR ', ') LIKE '%cheese%' THEN 'cheese'
         WHEN LOWER(r.recipe_name) LIKE '%chocolate%' OR GROUP_CONCAT(LOWER(i.name) SEPARATOR ', ') LIKE '%chocolate%' THEN 'chocolate'
         WHEN LOWER(r.recipe_name) LIKE '%apple%' OR GROUP_CONCAT(LOWER(i.name) SEPARATOR ', ') LIKE '%apple%' THEN 'apple'
+        WHEN LOWER(r.recipe_name) LIKE '%dates%' OR GROUP_CONCAT(LOWER(i.name) SEPARATOR ', ') LIKE '%dates%' THEN 'dates'
+        WHEN LOWER(r.recipe_name) LIKE '%avocado%' OR GROUP_CONCAT(LOWER(i.name) SEPARATOR ', ') LIKE '%avocado%' THEN 'avocado'
+        WHEN LOWER(r.recipe_name) LIKE '%apricot%' OR GROUP_CONCAT(LOWER(i.name) SEPARATOR ', ') LIKE '%apricot%' THEN 'apricot'
+        WHEN LOWER(r.recipe_name) LIKE '%nectarines%' OR GROUP_CONCAT(LOWER(i.name) SEPARATOR ', ') LIKE '%nectarines%' THEN 'nectarines'
+        WHEN LOWER(r.recipe_name) LIKE '%pomegranate%' OR GROUP_CONCAT(LOWER(i.name) SEPARATOR ', ') LIKE '%pomegranate%' THEN 'pomegranate'
         ELSE CONCAT('other-', r.recipe_id)
     END AS ingredient_family
 
@@ -47,3 +52,4 @@ SELECT *
 FROM curated_recipes
 ORDER BY recipe_id;
 
+SELECT COUNT(*) FROM curated_recipes;
