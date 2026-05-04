c9a49e45f73bf2646eaa475a32aee57e19675af1
Marim Ibrahim <6.mariam.h.6@gmail.com>
2026-05-04 22:54:15 +0400

    m

--- SQL (second version)/import_all_recipes.sql
+++ SQL (second version)/import_all_recipes.sql
@@ -20,7 +20,9 @@ CREATE TABLE recipes (
 
 CREATE TABLE users (
   user_id INT AUTO_INCREMENT PRIMARY KEY,
-  username VARCHAR(100) NOT NULL,
+  first_name VARCHAR(100) NOT NULL,
+  last_name VARCHAR(100) NOT NULL,
+  username VARCHAR(100) NOT NULL UNIQUE,
   password_hash VARCHAR(255) NOT NULL,
   email VARCHAR(255) NOT NULL UNIQUE
 );
